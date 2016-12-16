dcf0ee630a20b413d2212c3d3ae19ce4008a33fe
hembert <hembert@vauxoo.com>
2016-12-16 19:31:34 +0000

    [REF] openacademy: Modify copy method into inherit

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import models, fields
+from openerp import api, models, fields
 
 '''
 This module  create module of Courso
@@ -28,3 +28,17 @@ class Course(models.Model):
          'UNIQUE(name)',
          "The course title must be unique"),
     ]
+
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
