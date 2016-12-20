bbab8847a6b9f85960dcc4074aab04eff6d963e4
hembert <hembert@vauxoo.com>
2016-12-20 19:07:37 +0000

    [REF] openacademy: Remove old api of board view

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -32,6 +32,7 @@
         'security/ir.model.access.csv',
 	'view/openacademy_wizard_view.xml',
         'report/openacademy_session_report.xml',
+        'view/openacademy_session_board.xml',
         # 'templates.xml',
     ],
     # only loaded in demonstration mode
