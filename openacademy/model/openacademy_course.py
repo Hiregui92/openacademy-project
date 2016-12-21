ef96637caa699c1778185ea553fa0f8ce2baa0f7
hembert <hembert@vauxoo.com>
2016-12-21 12:49:10 +0000

    [FIX] openacademy: Standard pep8

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -12,21 +12,25 @@ class Course(models.Model):
 
     _name = 'openacademy.course'  # Model odoo name
 
-    name = fields.Char(string='Title', required=True)   # Field reserved to identified name rec
+    # Field reserved to identified name rec
+    name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one(
+        'res.users',
+        ondelete='set null',
+        string="Responsible",
+        index=True)
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
 
     _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
-        _("The title of the course should not be the description")),
+         "The title of the course should not be the description"),
 
         ('name_unique',
          'UNIQUE(name)',
-         _("The course title must be unique")),
+         "The course title must be unique"),
     ]
 
     @api.one
