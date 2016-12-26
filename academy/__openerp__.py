49c0a4b6a6f0dba88add06c1d242ad153460b85b
hembert <hembert@vauxoo.com>
2016-12-26 14:29:53 +0000

    [ADD] academy: Adding static content to be shown in the template

--- academy/__openerp__.py
+++ academy/__openerp__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        'views/academy_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
