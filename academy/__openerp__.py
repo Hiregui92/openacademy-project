32b983a180718d70891b75f0f8e507765ee041cf
hembert <hembert@vauxoo.com>
2016-12-26 17:12:59 +0000

    [ADD] academy: Adding view academy_teachers odoo template

--- academy/__openerp__.py
+++ academy/__openerp__.py
@@ -28,6 +28,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/academy_templates.xml',
+        'views/academy_teachers.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
